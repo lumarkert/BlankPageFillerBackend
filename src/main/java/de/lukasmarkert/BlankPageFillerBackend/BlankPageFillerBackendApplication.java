ce01533656b76897c5d48444d697247a0e6aabef
DESKTOP-1OSQ4FJ\lukas <lukasmarkert1996@gmail.com>
2021-02-24 02:27:04 +0100

    initial commit

---
+++ src/main/java/de/lukasmarkert/BlankPageFillerBackend/BlankPageFillerBackendApplication.java
@@ -0,0 +1,13 @@
+package de.lukasmarkert.BlankPageFillerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlankPageFillerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlankPageFillerBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/de/lukasmarkert/BlankPageFillerBackend/PdfService.java
@@ -0,0 +1,12 @@
+package de.lukasmarkert.BlankPageFillerBackend;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PdfService {
+
+    public void addBlankPages() {
+
+    }
+
+}
